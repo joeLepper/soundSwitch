aa2687fb4754125b75e9490f79b234987461a572
joeLepper <josephlepper@gmail.com>
2013-07-30 20:47:34 -0700

    achieve repeated blinks

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-var gpio = require('pi-gpio');
 
 
-gpio.open(16,'output',function(err) {
-  gpio.write(16,1,function() {
-    console.log('inside callback from write');
-    gpio.close(16);
-  });
-});
+var gpio = require('pi-gpio')
+  , isOn = false
+  , flash = function(){
+    isOn = !isOn;
+    setTimeout(function(){doFlash()},100);
+  }
+  , doFlash = function(){
+    gpio.open(22,'output',function(err) {
+      if(isOn){
+        gpio.write(22,1,function(){flash()})
+      }
+      else{
+        gpio.write(22,0,function(){flash()});
+      }
+    });
+  };
+gpio.close(22);
+setTimeout(function(){doFlash()},1000);
